35bc8b900d0c95201aff7424a36b9b5044917e4f
ParkSwap Inc. <60989363+ParkSwap-app@users.noreply.github.com>
2026-07-28 14:43:06 -0400

    Update service worker cache version to v16

--- static/app/sw.js
+++ static/app/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "parkswap-web-v15-destination-independent";
+const CACHE = "parkswap-web-v16-live-address";
 const SHELL = ["./", "app.css", "app.js", "vendor/leaflet/leaflet.css", "vendor/leaflet/leaflet.js", "manifest.webmanifest", "../parkswap-app-icon.png"];
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(SHELL)).then(() => self.skipWaiting())));
 self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())));
