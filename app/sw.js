b634b9ccc0287d7913b50667f264199315638a33
ParkSwap Inc. <60989363+ParkSwap-app@users.noreply.github.com>
2026-07-27 21:28:44 -0400

    Fix map location control visibility

--- app/sw.js
+++ app/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "parkswap-web-v7-network-flow";
+const CACHE = "parkswap-web-v8-location-control";
 const SHELL = ["./", "app.css", "app.js", "vendor/leaflet/leaflet.css", "vendor/leaflet/leaflet.js", "manifest.webmanifest", "../parkswap-app-icon.png"];
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(SHELL)).then(() => self.skipWaiting())));
 self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())));
